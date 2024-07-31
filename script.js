baf8a89e4d7f5b75712d755d4042913d2ca3ace4
SwethasriK <122242598+SwethasriKalaiselvan@users.noreply.github.com>
2024-07-31 16:12:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,22 @@
-        //javascript for navigation bar effects on scroll
-        window.addEventListener("scroll", function(){
-            const header = document.querySelector("header");
-            header.classList.toggle('sticky', window.scrollY > 0);
-          });
-          
-          //javascript for responsive navigation sidebar menu
-          const menuBtn = document.querySelector(".menu-btn");
-          const navigation = document.querySelector(".navigation");
-          const navigationItems = document.querySelectorAll(".navigation a")
-          
-          menuBtn.addEventListener("click",  () => {
-            menuBtn.classList.toggle("active");
-            navigation.classList.toggle("active");
-          });
-          
-          navigationItems.forEach((navigationItem) => {
-            navigationItem.addEventListener("click", () => {
-              menuBtn.classList.remove("active");
-              navigation.classList.remove("active");
-            });
-          });
-          
-          //javascript for scroll to top button
-          const scrollBtn = document.querySelector(".scrollToTop-btn");
-          
-          window.addEventListener("scroll", function(){
-            scrollBtn.classList.toggle("active", window.scrollY > 500);
-          });
-          
-          //javascript for scroll back to top on click scroll-to-top button
-          scrollBtn.addEventListener("click", () => {
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-          });
-          
-          //javascript for reveal website elements on scroll
-          window.addEventListener("scroll", reveal);
-          
-          function reveal(){
-            var reveals = document.querySelectorAll(".reveal");
-          
-            for(var i = 0; i < reveals.length; i++){
-              var windowHeight = window.innerHeight;
-              var revealTop = reveals[i].getBoundingClientRect().top;
-              var revealPoint = 50;
-          
-              if(revealTop < windowHeight - revealPoint){
-                reveals[i].classList.add("active");
-              }
-            }
-          }
-                
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset+height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    };
+  });
+  
+  let header = document.querySelector('.header');
+  header.classList.toggle('sticky', window.scrollY > 100);
+};
+
