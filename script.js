360dc795d9c770ab7d5f63f595a120657000022f
SwethasriK <122242598+SwethasriKalaiselvan@users.noreply.github.com>
2024-08-10 00:04:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/*-------menu icon navbar-------*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+/*-------scroll sections-------*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -16,7 +26,12 @@ window.onscroll = () => {
     };
   });
   
+  /*-------sticky navbar-------*/
   let header = document.querySelector('.header');
   header.classList.toggle('sticky', window.scrollY > 100);
+  
+  /*-------remove menu icon navbar (scroll)-------*/
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
 };
 
